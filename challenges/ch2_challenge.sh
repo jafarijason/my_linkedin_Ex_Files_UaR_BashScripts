c3462468ef912088289dd33d289087b0612df690
JasonJafariHome <jason.jafari.m@gmail.com>
2020-04-13 00:54:33 -0400

    first commit

---
+++ challenges/ch2_challenge.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# A challenge for Chapter 2 of Up and Running with Bash Scripts
+#
+# Use some variables, some command substitution, and some string formatting
+#+to make a small summary of some system information. Print the result to
+#+the screen and write some information to a file.
---
+++ challenges/ch4_challenge.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# A challenge for Chapter 4 of Up and Running with Bash Scripts
+#
+# Write a simple guessing game, using interactive input and a test condition. 
+#+Also, build the program so that it responds to a command line argument
+#+and also has a function if no argument is specified. Use a function as well.
